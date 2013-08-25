a4bbf69a97d70e680d25eebd80c43a303df557a2
splaktar <splaktar@gmail.com>
2013-08-25 18:26:51 -0400

    First commit

---
+++ gdgspacecoast/src/org/gdgspacecoast/GdgspacecoastServlet.java
@@ -0,0 +1,11 @@
+package org.gdgspacecoast;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GdgspacecoastServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
---
+++ gdgspacecoast/war/js/config.js
@@ -0,0 +1,17 @@
+boomerang.factory('Config', function () {
+    return {
+        //modify these
+        'name'          : 'GDG Space Coast',
+        'id'            : '103959793061819610212',
+        'google_api'    : 'AIzaSyBoJOzKrUyT4ShFVWvMWvwpNP-jPnhgZJs',
+        'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
+        'cover' : {
+            title : 'GWT.create',
+            subtitle : 'The largest event about GWT in 2013 with 2 days and more than 60 presentations.',
+            button : {
+                text : 'Register',
+                url : 'http://gwtcreate.com/'
+            }
+        }
+    };
+});

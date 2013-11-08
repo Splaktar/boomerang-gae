bf16f7159f43bf7fa40b8669ca845e483b26f7d8
Michael Prentice <splaktar@gmail.com>
2013-11-07 19:32:41 -0500

    Merge in Eric's changes to implement the hashcode linkify feature and to include both the Google+ and developers.google.com links on the event page.

--- gdgspacecoast/war/js/boomerang.js
+++ gdgspacecoast/war/js/boomerang.js
@@ -210,3 +210,43 @@ boomerang.filter('htmlLinky', function($sanitize, linkyFilter) {
 		return linkify(inputDOM).innerHTML;
 	};
 });
+
+boomerang.filter('hashLinky', function($sanitize, linkyFilter) {
+    var ELEMENT_NODE = 1;
+    var TEXT_NODE = 3;
+    var linkifiedDOM = document.createElement('div');
+    var inputDOM = document.createElement('div');
+
+    var hashLinky = function hashLinky(startNode) {
+        var i, currentNode;
+
+        for (i = 0; i < startNode.childNodes.length; i++) {
+            currentNode = startNode.childNodes[i];
+
+            switch (currentNode.nodeType) {
+                case ELEMENT_NODE:
+                    hashLinky(currentNode);
+                    break;
+                case TEXT_NODE:
+                    var hashtagRegex = /#([A-Za-z0-9-_]+)/g;
+                    currentNode.textContent =  currentNode.textContent.replace(hashtagRegex,
+                        '<a href="https://plus.google.com/s/%23$1">#$1</a>');
+
+                    linkifiedDOM.innerHTML = currentNode.textContent;
+                    i += linkifiedDOM.childNodes.length - 1;
+
+                    while(linkifiedDOM.childNodes.length) {
+                        startNode.insertBefore(linkifiedDOM.childNodes[0], currentNode);
+                    }
+                    startNode.removeChild(currentNode);
+            }
+        }
+
+        return startNode;
+    };
+
+    return function(input) {
+        inputDOM.innerHTML = input;
+        return hashLinky(inputDOM).innerHTML;
+    };
+});
